22f635013e6d5d6ca47b7dc1cd31856a69546df4
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-18 14:36:56 +0300

    chore: update dependencies in pubspec.yaml and pubspec.lock, enhance AppColors and AppTextStyles with new color and text style definitions

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -6,8 +6,16 @@ class AppColors {
   static const Color black = Color(0xFF090909);
   static const Color white = Color(0xFFFFFFFF);
   static const Color grey = Color(0xFF9F9F9F);
+  static const Color transparent = Colors.transparent;
 
-  // text colors
+  // Background colors
+  static const Color backgroundLight = Color(0xFFF5F5F5);
+  static const Color backgroundTealLight = Color(0xFFE8F5F4);
+
+  // Accent colors
+  static const Color red = Color(0xFFFF6B6B);
+
+  // Text colors
   static const Color textPrimary = Color(0xFF000000);
   static const Color textSecondary = Color(0xFF646464);
 }
--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -5,8 +5,20 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class AppTextStyles {
   const AppTextStyles._();
 
+  static final TextStyle font20W700Black = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
   static final TextStyle font32W700Black = TextStyle(
     fontSize: 32.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static final TextStyle font16W700Black = TextStyle(
+    fontSize: 16.sp,
     fontWeight: FontWeight.w700,
     color: AppColors.black,
   );
@@ -15,5 +27,41 @@ class AppTextStyles {
     fontSize: 16.sp,
     fontWeight: FontWeight.w400,
     color: AppColors.grey,
+  );
+
+  static final TextStyle font14W500White = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.white,
+  );
+
+  static final TextStyle font14W500textSecondary = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.textSecondary,
+  );
+
+  static final TextStyle font14W400Grey = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.grey,
+  );
+
+  static final TextStyle font16W700Black2 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static final TextStyle font12W400TextSecondary = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+
+  static final TextStyle font11W400TextSecondary = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
   );
 }
